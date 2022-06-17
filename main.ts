3ec41f69baf33c35f305f97cee25774cad6aaf59
joao-muchatt-2010 <107720662+joao-muchatt-2010@users.noreply.github.com>
2022-06-17 19:49:31 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+        velocidade += -25
+    } else {
+        game.removeLife(1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (pausa == 0) {
+        pausa = 1
+    } else {
+        pausa = 0
+    }
+})
+let pausa = 0
+let sprite: game.LedSprite = null
+let on = 1
+let velocidade = 500
+game.setLife(4)
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    if (on == 1) {
+        sprite.move(1)
+        sprite.ifOnEdgeBounce()
+        basic.pause(velocidade)
+    }
+})
+basic.forever(function () {
+    if (pausa == 1) {
+        game.pause()
+        basic.clearScreen()
+    } else {
+        game.resume()
+    }
+})
